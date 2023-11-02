24c325230b493703741bae47893f6bdfdc9ccab4
Mohamed Nasr <mhmdnsr.dev@gmail.com>
2023-11-02 18:19:59 +0200

    feat: Implemented user profile type selection during signup
    Added functionality to allow users to specify their profile type during the signup process, enabling better personalization and customization of their profiles.

--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -1,62 +1,89 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
-import { FormBuilder, FormGroup, Validators, FormControl, AbstractControl } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  FormControl,
+  AbstractControl,
+} from '@angular/forms';
 
 // matching passwords
-function passwordMatchValidator(control: AbstractControl): { [key: string]: boolean } | null {
+function passwordMatchValidator(
+  control: AbstractControl
+): { [key: string]: boolean } | null {
   const password = control.get('password');
   const confirmPassword = control.get('confirmPassword');
-
-  if (!password || !confirmPassword) return null;
-
+  if (!password?.value || !confirmPassword?.value) return null;
   if (password.value !== confirmPassword.value) {
-    return { 'passwordMismatch': true };
+    return { passwordMismatch: true };
   }
-
   return null;
 }
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.scss']
+  styleUrls: ['./signup.component.scss'],
 })
-export class SignupComponent implements OnInit{
-   registrationForm!: FormGroup;
+export class SignupComponent implements OnInit {
+  registrationForm!: FormGroup;
 
-  constructor(private fb: FormBuilder,private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router) {}
 
   ngOnInit(): void {
-    this.registrationForm = this.fb.group({
-      email: ['', [Validators.required, Validators.email]],
-      name: ['', [Validators.required, Validators.minLength(3)]],
-      username: ['', [Validators.required, Validators.pattern(/^\S*$/)]],
-      password: [
-        '',
-        [
-          Validators.required,
-          Validators.minLength(8),
-          Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@*%$#])[A-Za-z\d@*%$#]+$/)
-        ]
-      ],
-      confirmPassword: ['', Validators.required]
-    }, {
-      validator: passwordMatchValidator
-    });
+    this.registrationForm = this.fb.group(
+      {
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/
+            ),
+          ],
+        ],
+        name: ['', [Validators.required, Validators.minLength(3)]],
+        username: ['', [Validators.required, Validators.pattern(/^\S*$/)]],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$/
+            ),
+          ],
+        ],
+        confirmPassword: [
+          '',
+          [
+            Validators.required,
+            Validators.pattern(
+              /^(?=[A-Za-z0-9@#$%^&+!=]+$)^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+!=])(?=.{8,}).*$/
+            ),
+          ],
+        ],
+        profile: ['client'],
+      },
+      {
+        validator: passwordMatchValidator,
+      }
+    );
   }
 
   onSubmit() {
     if (this.registrationForm.valid) {
-      console.log('Registration successful!');
+      // console.log('Registration successful!');
+      delete this.registrationForm.value.confirmPassword;
       console.log(this.registrationForm.value);
+      // this.redirectToLogin();
     } else {
       console.log('Form has validation errors.');
     }
   }
 
-   redirectToLogin() {
+  redirectToLogin() {
     this.router.navigate(['login']);
   }
 }
