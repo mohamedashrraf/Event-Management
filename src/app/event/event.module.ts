5c7043b8fbba22e7d26668b3cc5ee0792a3f89dd
MohaMed AshRaf <mohmmedashrraf@gmail.com>
2023-11-05 15:45:00 +0200

    resolve conflect

--- src/app/event/event.module.ts
+++ src/app/event/event.module.ts
@@ -4,18 +4,25 @@ import { EventHomeComponent } from './event-home/event-home.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { LandingComponent } from './landing/landing.component';
 import { ProfileComponent } from './profile/profile.component';
-
+import { PlacesComponent } from './places/places.component';
+import { EventsComponent } from './events/events.component';
+import { AboutsComponent } from './abouts/abouts.component';
+import { AppRoutingModule } from '../app-routing.module';
 
 
 @NgModule({
-    declarations: [
-        EventHomeComponent,
-        LandingComponent,
-        ProfileComponent
-    ],
-    imports: [
-        CommonModule,
-        CalendarComponent
-    ]
+  declarations: [
+    EventHomeComponent,
+    LandingComponent,
+    ProfileComponent,
+    PlacesComponent,
+    EventsComponent,
+    AboutsComponent
+  ],
+  imports: [
+    CommonModule,
+    AppRoutingModule,
+    CalendarComponent,
+  ]
 })
 export class EventModule { }
