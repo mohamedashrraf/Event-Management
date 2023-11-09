b0f9f786d7e2ab8a092f0e25dd52b61f6718a9d2
Muhammad Shaker Kasem AlKilany <m.sh64944@gmail.com>
2023-11-09 18:55:06 +0200

    get user img

--- src/app/event/profile/profile.component.ts
+++ src/app/event/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import UserInfo from './interfaces/userInfo';
-
+import { jwtDecode } from 'jwt-decode';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -9,16 +9,24 @@ import UserInfo from './interfaces/userInfo';
 })
 export class ProfileComponent {
   userInfo!: UserInfo;
+  imgSrc:string="http://localhost:4000/api/v1/user/ProPicPath/"
   constructor(private authService: AuthService) {
     this.authService.user.subscribe((user) => {
       !user.isAuthenticated && this.authService.redirectToLogin();
       this.userInfo = user;
     });
+    const whoiam = localStorage.getItem('whoiam');
+  const token = JSON.parse(whoiam!).token;
+
+      console.log(jwtDecode(token!))
+      const tokenData = jwtDecode(token!)as any
+      this.imgSrc+=tokenData._id
   }
 
   changePhoto(event: Event) {
     const targetEl = event.target as HTMLElement;
     const clickEvent = new MouseEvent('click');
     targetEl.children.item(0)?.dispatchEvent(clickEvent);
+  
   }
 }
