0d7f8d60d9a92ac0fd0cff42bef52c6e11cc421d
Muhammad Shaker Kasem AlKilany <m.sh64944@gmail.com>
2023-11-09 19:45:27 +0200

    chang user photo

--- src/app/event/profile/profile.component.ts
+++ src/app/event/profile/profile.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { AuthService } from 'src/app/auth/auth.service';
 import UserInfo from './interfaces/userInfo';
 import { jwtDecode } from 'jwt-decode';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
@@ -10,7 +11,7 @@ import { jwtDecode } from 'jwt-decode';
 export class ProfileComponent {
   userInfo!: UserInfo;
   imgSrc:string="http://localhost:4000/api/v1/user/ProPicPath/"
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService,private httpClint:HttpClient) {
     this.authService.user.subscribe((user) => {
       !user.isAuthenticated && this.authService.redirectToLogin();
       this.userInfo = user;
@@ -23,10 +24,30 @@ export class ProfileComponent {
       this.imgSrc+=tokenData._id
   }
 
-  changePhoto(event: Event) {
+  changePhoto(event: any) {
     const targetEl = event.target as HTMLElement;
     const clickEvent = new MouseEvent('click');
     targetEl.children.item(0)?.dispatchEvent(clickEvent);
-  
+    
+    
+  }
+  sendNewPhoto(event: any){
+
+    const file = event.target.files[0]
+    const formData = new FormData()
+    formData.append('proPic', file)
+    this.httpClint.patch("http://localhost:4000/api/v1/user/",formData).subscribe((res)=>{
+      console.log(res)
+      const reader = new FileReader();
+
+
+  reader.onload = (event:any)=>{
+    this.imgSrc  = event.target.result! as string;
+     
+  };
+
+  reader.readAsDataURL(file)
+   })
+    
   }
 }
