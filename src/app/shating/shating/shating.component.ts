a8658f3119c6ae9fe8cb56885cef81cbecccc9cd
Muhammad Shaker Kasem AlKilany <m.sh64944@gmail.com>
2023-11-09 08:39:34 +0200

    shating init

---
+++ src/app/shating/shating/shating.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shating',
+  templateUrl: './shating.component.html',
+  styleUrls: ['./shating.component.scss']
+})
+export class ShatingComponent {
+
+}
