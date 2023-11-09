1670bad2248b1aee86c2f505d28122a4eeefe15e
Muhammad Shaker Kasem AlKilany <m.sh64944@gmail.com>
2023-11-09 08:48:14 +0200

    eventId

--- src/app/shating/shating/shating.component.ts
+++ src/app/shating/shating/shating.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-shating',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shating.component.scss']
 })
 export class ShatingComponent {
+eventId: string;
+constructor(private activeRoute:ActivatedRoute){
 
+  this.eventId = this.activeRoute.snapshot.params["id"]
+}
 }
