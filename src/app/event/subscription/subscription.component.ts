0d01f74c1e62226a1eec1b0e1210068f5c925d88
Your Nameomar <omargamal.2tecno@gmail.com>
2023-11-11 11:45:35 +0200

    fix conflect in subscribe

--- src/app/event/subscription/subscription.component.ts
+++ src/app/event/subscription/subscription.component.ts
@@ -7,43 +7,27 @@ import { EventHttpService } from '../services/event-http.service';
 @Component({
   selector: 'app-subscription',
   templateUrl: './subscription.component.html',
-  styleUrls: ['./subscription.component.scss']
+  styleUrls: ['./subscription.component.scss'],
 })
 export class SubscriptionComponent {
-  isGitLink:boolean=false
-  constructor(private eventHttp:EventHttpService,private tokenData:GetTokenDataService){}
-VIPPaypal() {
-  this.isGitLink = true
-this.eventHttp.getPaypal(this.tokenData.tokenData._id).subscribe((res)=>{
-  console.log(res.data.link)
-  const link = res.data.link
-  location.href = link
-  // this.isGitLink = false
-});
-}
-
-  activeId:any
-  vip: any;
-  constructor(private activeRoute: ActivatedRoute,private router: Router, private getTokenData: GetTokenDataService, private eventHttp: EventHttpService) {
-
-     this.activeId = this.getTokenData.tokenData._id;
+  isGitLink: boolean = false;
+  activeId: any;
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private router: Router,
+    private getTokenData: GetTokenDataService,
+    private eventHttp: EventHttpService
+  ) {
+    this.activeId = this.getTokenData.tokenData._id;
     console.log(this.activeId);
   }
-
-  ngOnInit() {
-
-    this.eventHttp.vipPlan(this.activeId).subscribe(
-      (res) => {
-        this.vip = res.data;
-        console.log(this.vip);
-        // location.href = this.vip;
-      },
-      (error) => {
-        console.log(error);
-          // this.router.navigate(['/notfound']);
-      }
-    )
+  VIPPaypal() {
+    this.isGitLink = true;
+    this.eventHttp.getPaypal(this.getTokenData.tokenData._id).subscribe((res) => {
+      console.log(res.data.link);
+      const link = res.data.link;
+      location.href = link;
+      // this.isGitLink = false
+    });
   }
-
-
 }
