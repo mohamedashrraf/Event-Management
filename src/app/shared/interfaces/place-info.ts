4506e333d146f8182e8f232367eed6bf6f9d2a2d
Mohamed Nasr <mhmdnsr.dev@gmail.com>
2023-11-11 23:54:09 +0200

    t

---
+++ src/app/shared/interfaces/place-info.ts
@@ -0,0 +1,11 @@
+export default interface PlaceInfo {
+  address: string;
+  admins: string[];
+  category: string;
+  createdAt: string;
+  description: string;
+  updatedAt: string;
+  googleMapUrl: string;
+  placPhoto: string;
+  _id: string;
+}
