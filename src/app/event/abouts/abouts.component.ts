de3e8f2cba5cfba9a02568c6c5acc98aaca1b13d
MohaMed AshRaf <mohmmedashrraf@gmail.com>
2023-11-13 02:44:19 +0200

    update about user details

--- src/app/event/abouts/abouts.component.ts
+++ src/app/event/abouts/abouts.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/auth/auth.service';
-import { User } from 'src/app/shared/interfaces/user';
 import UserInfo from '../../shared/interfaces/user-info';
 import { jwtDecode } from 'jwt-decode';
 
@@ -12,6 +11,8 @@ import { jwtDecode } from 'jwt-decode';
 })
 export class AboutsComponent implements OnInit {
   user!: UserInfo;
+  editMode: boolean = false;
+
   constructor(private router: Router, private authService: AuthService) {
     const whoiam = localStorage.getItem('whoiam');
     // if (!whoiam) this.router.navigate(['/login']);
@@ -21,10 +22,20 @@ export class AboutsComponent implements OnInit {
       !user.isAuthenticated && this.authService.redirectToLogin();
 
       this.user = user;
-      console.log(jwtDecode(this.user.token!));
       const tokenData = jwtDecode(this.user.token!) as any;
       this.user = { ...this.user, ...tokenData };
     });
   }
+
   ngOnInit(): void {}
+
+  editProfile() {
+    this.editMode = true;
+  }
+
+  saveChanges() {
+    // After saving, set editMode to false.
+    this.editMode = false;
+  }
 }
+
